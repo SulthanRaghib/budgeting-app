0a2248d12d34321880e17b37851a7b0d9fa1c391
SulthanRaghib <raghib.smp@gmail.com>
2025-12-14 17:13:32 +0700

    feat: add getRedirectUrl method to Create and Edit SavingTransaction pages

--- app/Filament/Resources/SavingTransactions/Pages/CreateSavingTransaction.php
+++ app/Filament/Resources/SavingTransactions/Pages/CreateSavingTransaction.php
@@ -8,4 +8,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateSavingTransaction extends CreateRecord
 {
     protected static string $resource = SavingTransactionResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
